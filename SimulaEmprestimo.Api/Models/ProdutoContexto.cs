3cc030b8620a97aeb01757c88e397d2dada4423b
Júnior Almeida <jun10r4lm31d4@outlook.com>
2025-09-21 18:26:23 -0300

    Refactor

--- SimulaEmprestimo.Api/Models/ProdutoContexto.cs
+++ SimulaEmprestimo.Api/Models/ProdutoContexto.cs
@@ -4,10 +4,7 @@ namespace SimulaEmprestimo.Api.Models
 {
     public class ProdutoContexto : DbContext
     {
-        public ProdutoContexto(DbContextOptions<ProdutoContexto> options) : base(options)
-        {
-        }
-
+        public ProdutoContexto(DbContextOptions<ProdutoContexto> options) : base(options) { }
         public DbSet<Produto> Produtos { get; set; }
     }
 }
